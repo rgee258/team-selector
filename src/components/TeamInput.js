8b11b33639d0b75a5b1e318832277aeacbb71838
rgee258 <9219093+rgee258@users.noreply.github.com>
2020-11-11 01:21:42 -0500

    Completed basic team information form with state and added react-fontawesome

--- src/components/TeamInput.js
+++ src/components/TeamInput.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import '../css/styles.css';
 
 class TeamInput extends Component {
@@ -8,13 +10,9 @@ class TeamInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      names: ['', '', '', '', '', '', '', '']
+      names: ['', '', '', '', '', '', '', ''],
+      separations: ['', '', '', '', '', '']
     };
-  }
-
-  getName = (e) => {
-    const nameIndex = parseInt(e.target.id.match(/\d+/).pop());
-    return this.state.names[nameIndex];
   }
 
   setName = (e) => {
@@ -30,51 +28,174 @@ class TeamInput extends Component {
 
       return { names };
     });
-    console.log(this.state.names);
+  }
+
+  setSeparation = (e) => {
+    const separationIndex = parseInt(e.target.id.match(/\d+/).pop());
+    this.setState(state => {
+      const separations = state.separations.map((separation, currIndex) => {
+        if (currIndex === separationIndex) {
+          return e.target.value;
+        } else {
+          return separation;
+        }
+      });
+
+      return { separations };
+    });
   }
 
   render() {
     return (
-      <div className="teamForm">
-        <h2>Team Information</h2>
+      <Container className="teamForm text-center">
+        <h2 className="mt-3">Team Information</h2>
+        {/* TODO: Add onSubmit from props */}
         <Form>
-          <Form.Group controlId="formNames">
-            <Form.Label>Member Names</Form.Label>
-            <Form.Row>
+          <Form.Group className="formNames">
+            <Form.Label className="formNamesLabel mb-3 mt-3">Member Names</Form.Label>
+            <Form.Row className="formNamesRow">
               <Col md="1"></Col>
               <Col>
-                <Form.Control id="formNameInput0" as="input" placeholder="Name" onChange={ this.setName } value={ this.state.names[0] }/>
+                <Form.Control id="formNameInput0" as="input" placeholder="Name" onChange={this.setName} value={this.state.names[0]}/>
               </Col>
               <Col>
-                <Form.Control id="formNameInput1" as="input" placeholder="Name" onChange={ this.setName } value={ this.state.names[1] }/>
+                <Form.Control id="formNameInput1" as="input" placeholder="Name" onChange={this.setName} value={this.state.names[1]}/>
               </Col>
               <Col>
-                <Form.Control id="formNameInput2" as="input" placeholder="Name" onChange={ this.setName } value={ this.state.names[2] }/>
+                <Form.Control id="formNameInput2" as="input" placeholder="Name" onChange={this.setName} value={this.state.names[2]}/>
               </Col>
               <Col>
-                <Form.Control id="formNameInput3" as="input" placeholder="Name" onChange={ this.setName } value={ this.state.names[3] }/>
+                <Form.Control id="formNameInput3" as="input" placeholder="Name" onChange={this.setName} value={this.state.names[3]}/>
               </Col>
               <Col md="1"></Col>
             </Form.Row>
-            <Form.Row>
+            <Form.Row className="formNamesRow mt-2">
               <Col md="1"></Col>
               <Col>
-                <Form.Control id="formNameInput4" as="input" placeholder="Name" onChange={ this.setName } value={ this.state.names[4] }/>
+                <Form.Control id="formNameInput4" as="input" placeholder="Name" onChange={this.setName} value={this.state.names[4]}/>
               </Col>
               <Col>
-                <Form.Control id="formNameInput5" as="input" placeholder="Name" onChange={ this.setName } value={ this.state.names[5] }/>
+                <Form.Control id="formNameInput5" as="input" placeholder="Name" onChange={this.setName} value={this.state.names[5]}/>
               </Col>
               <Col>
-                <Form.Control id="formNameInput6" as="input" placeholder="Name" onChange={ this.setName } value={ this.state.names[6] }/>
+                <Form.Control id="formNameInput6" as="input" placeholder="Name" onChange={this.setName} value={this.state.names[6]}/>
               </Col>
               <Col>
-                <Form.Control id="formNameInput7" as="input" placeholder="Name" onChange={ this.setName } value={ this.state.names[7] }/>
+                <Form.Control id="formNameInput7" as="input" placeholder="Name" onChange={this.setName} value={this.state.names[7]}/>
               </Col>
               <Col md="1"></Col>
             </Form.Row>
           </Form.Group>
+          <Form.Group className="formSeparations">
+            <Form.Label className="formSeparationsLabel mb-3 mt-3">Separated Members</Form.Label>
+            <Form.Row className="formSeparationsRow">
+              <Col md="3"></Col>
+              <Col>
+                <Form.Control id="formNameSeparate0" as="select" onChange={this.setSeparation} value={this.state.separations[0]}>
+                  <option key="separateOption0"></option>
+                  { 
+                    this.state.names.filter((name) => {
+                      if (name !== '') { return true; }
+                      return false;
+                    }).map((name, index) => {
+                      return (
+                        <option key={`separateOption${index + 1}`}>{name}</option>
+                      );
+                    }) 
+                  }
+                </Form.Control>
+              </Col>
+              <Col>
+                <Form.Control id="formNameSeparate1" as="select" onChange={this.setSeparation} value={this.state.separations[1]}>
+                  <option key="separateOption0"></option>
+                  { 
+                    this.state.names.filter((name) => {
+                      if (name !== '') { return true; }
+                      return false;
+                    }).map((name, index) => {
+                      return (
+                        <option key={`separateOption${index + 1}`}>{name}</option>
+                      );
+                    }) 
+                  }
+                </Form.Control>
+              </Col>
+              <Col md="3"></Col>
+            </Form.Row>
+            <hr className="formSeparationBreak" />
+            <Form.Row className="formSeparationsRow">
+              <Col md="3"></Col>
+              <Col>
+                <Form.Control id="formNameSeparate2" as="select" onChange={this.setSeparation} value={this.state.separations[2]}>
+                  <option key="separateOption0"></option>
+                  { 
+                    this.state.names.filter((name) => {
+                      if (name !== '') { return true; }
+                      return false;
+                    }).map((name, index) => {
+                      return (
+                        <option key={`separateOption${index + 1}`}>{name}</option>
+                      );
+                    }) 
+                  }
+                </Form.Control>
+              </Col>
+              <Col>
+                <Form.Control id="formNameSeparate3" as="select" onChange={this.setSeparation} value={this.state.separations[3]}>
+                  <option key="separateOption0"></option>
+                  { 
+                    this.state.names.filter((name) => {
+                      if (name !== '') { return true; }
+                      return false;
+                    }).map((name, index) => {
+                      return (
+                        <option key={`separateOption${index + 1}`}>{name}</option>
+                      );
+                    }) 
+                  }
+                </Form.Control>
+              </Col>
+              <Col md="3"></Col>
+            </Form.Row>
+            <hr className="formSeparationBreak" />
+            <Form.Row className="formSeparationsRow">
+              <Col md="3"></Col>
+              <Col>
+                <Form.Control id="formNameSeparate4" as="select" onChange={this.setSeparation} value={this.state.separations[4]}>
+                  <option key="separateOption0"></option>
+                  { 
+                    this.state.names.filter((name) => {
+                      if (name !== '') { return true; }
+                      return false;
+                    }).map((name, index) => {
+                      return (
+                        <option key={`separateOption${index + 1}`}>{name}</option>
+                      );
+                    }) 
+                  }
+                </Form.Control>
+              </Col>
+              <Col>
+                <Form.Control id="formNameSeparate5" as="select" onChange={this.setSeparation} value={this.state.separations[5]}>
+                  <option key="separateOption0"></option>
+                  { 
+                    this.state.names.filter((name) => {
+                      if (name !== '') { return true; }
+                      return false;
+                    }).map((name, index) => {
+                      return (
+                        <option key={`separateOption${index + 1}`}>{name}</option>
+                      );
+                    }) 
+                  }
+                </Form.Control>
+              </Col>
+              <Col md="3"></Col>
+            </Form.Row>
+          </Form.Group>
+          <Button className="form-submit" type="submit">Create Team</Button>
         </Form>
-      </div>
+      </Container>
     );
   }
 }
